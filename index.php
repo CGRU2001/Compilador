3df9be21a0fac91447fe4382f328ffa26a7b66fa
Carlos <carlosguillermoru@gmail.com>
2020-10-28 14:24:34 +0100

    Implementa CSS

--- index.php
+++ index.php
@@ -38,6 +38,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"/>
     <title>Compilador</title>
 </head>
 <body>
