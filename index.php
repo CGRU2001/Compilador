91168fd8f4ee54c0f6a3021ed48755564ec4c72e
Carlos <carlosguillermoru@gmail.com>
2020-10-29 10:17:27 +0100

    Se reestructura todo el código

--- index.php
+++ index.php
@@ -1,80 +1,129 @@
 <?php
-    $line = 0;
+    if (isset($_POST["code"])){
+        $code=strtolower($_POST["code"]);
 
-    function checkline($may){
-        for ($i=0; $i < count($may); $i++) { 
-            $GLOBALS['line'] = ($i+1);
+        $codefixed=explode("\n",$code);
+    }
 
-            if(!(checkVal($may[$i]))){
-                return false;
+
+    function buscar_errores($code){
+        $errores=array();
+        $opened=array();
+        $ifopened=false;
+        foreach ($code as $clave => $line){
+            $linea=explode(" ",$line);
+            if (trim($linea[0])=="if"||trim($linea[0])=="for"||trim($linea[0])=="while"||trim($linea[0])=="end"||trim($linea[0])=="else"||trim($linea[1]=='=')){
+                if($linea[1]=='='){
+                    $ifopened = true;
+                }
+                switch (trim($linea[0])){
+                    case "if":
+                        array_push($opened,$clave+1);
+                        $ifopened=true;
+                        break;
+                    case "for":
+                    case "while":
+                        array_push($opened,$clave+1);
+                        break;
+                    case "end":
+                        array_shift($opened);
+                        break;
+                    case "else":
+                        if ($ifopened==false){
+                            array_push($errores,$clave+1);
+                        }
+                        break;
+                }
+            }else{
+                if (count($linea)>2&&(strpos($linea[0], '=') == false||strpos($linea[1], '=') == false)){
+                    array_push($errores,$clave+1);
+                }else{
+                    switch (trim($linea[0])){
+                        case "print":
+                        case "tr":
+                        case "tl":
+                        case "deact":
+                        case "a":
+                        case "":
+                            break;
+                        default:
+                            if (strpos($linea[0], '=') != false||strpos($linea[1], '=') != false){
+                                if (strpos($linea[0], '=') != false){
+                                    $variable=substr($linea[0], 0, strpos($linea[0],'='));
+                                    if ($variable!=""){
+                                        if (!ctype_alnum(str_replace('_', '', $variable))||!ctype_alpha($variable[0])){
+                                            array_push($errores,$clave+1);
+                                            break;
+                                        }else{
+                                            break;
+                                        }
+                                    }else{
+                                        array_push($errores,$clave+1);
+                                        break;
+                                    }
+                                }else{
+                                    $variable=$linea[0];
+                                    if ($variable!=""){
+                                        if (!ctype_alnum(str_replace('_', '', $variable))||!ctype_alpha($variable[0])){
+                                            array_push($errores,$clave+1);
+                                            break;
+                                        }else{
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            elseif (strpos($linea[0], '++') != false||strpos($linea[0], '--') != false){
+                                break;
+                            }
+                            else{
+                                array_push($errores,$clave+1);
+                                break;
+                            }
+                    }
+                }
             }
-            
         }
-        return true;
-    }
-
-    function checkVal($x){
-        $txt = array('A', 'TR', 'TL', 'DEAC', 'PRINT');
-        $vals = explode(' ',$x);
-        print_r($vals);
-        for ($i=0; $i < count($vals); $i++) { 
-                if($vals[0]==$txt[$i] && @$vals[1] == '='){
-                    return false;
-                }
-                if('A' == $vals[$i] && count($vals) == 1){
-                    return true;
-                }
-                if('TR' == $vals[$i] && count($vals) == 1){
-                    return true;
-                }if('TL' == $vals[$i] && count($vals) == 1){
-                    return true;
-                }if('DEAC' == $vals[$i] && count($vals) == 1){
-                    return true;
-                }if('PRINT' == $vals[$i] && count($vals) == 2){
-                    return true;
-                }if(@$vals[1] == '='){
-                    return true;
-                }
-
-            
+        if (count($errores)>0||count($opened)>0){
+            foreach ($errores as $valor){
+                echo "<br>Error en la linea ".$valor;
+            }
+            foreach ($opened as $valor){
+                echo "<br>Error en la linea ".$valor;
+            }
         }
-        return false;
+        else{
+            echo "Codigo correcto";
+        }
     }
-
 ?>
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet"/>
-    <title>Compilador</title>
-</head>
-<body>
-    <h1>Aprende a programar con demeter</h1>
-    <form method="post" action="index.php">
-        <label for="code">Inserta el código fuente aquí</label>
-        <textarea id="code" name="sourcecode"><?php if(isset($_POST['sourcecode'])){echo $_POST['sourcecode'];} ?></textarea>
-        <input type="submit" name="send" value="comprobar"/>
-    </form>
-    <?php
+    <html>
+        <head>
+            <title>Compilador</title>
+            <meta charset="UTf-8"/>
+            <link rel="stylesheet" href="css/style.css"/>
+        </head>
+        <body>
+            <div style="display: flex;flex-direction: column;justify-content: center;align-items: center;">
+                <h1>Demeter</h1>
+                <form method="post" action="index.php">
+                    <label for="code">Inserta el código fuente aquí</label>
+                    <textarea id="code" name="code"><?php if(isset($_POST['send'])){echo $_POST['code'];} ?></textarea>
+                    <input type="submit" name="send" value="comprobar"/>
+                    <?php
+                    if(isset($_POST["send"]))
+                    {
+                        if($_POST["code"]=="") {
+                            echo "<br>Error: No se ha escrito nada";
+                        }else{
+                            buscar_errores($codefixed);
+                        }
 
-        if(isset($_POST['send'])){
-            if($_POST['sourcecode'] == ''){
-                echo 'No se ha introducido nada';
-            }else{
-                $may = strtoupper($_POST['sourcecode']);
-                $may = explode(PHP_EOL, $may);
-                if(checkline($may)){
-                    echo '<p>El código es válido</p>';
-                }else{
-                    echo '<p>Error en la línea '.$line.'</p>';
-                }
-                
-            }
-        }
 
-    ?>
-</body>
+                    }
+                    ?>
+                </form>
+            </div>
+        </body>
 </html>
